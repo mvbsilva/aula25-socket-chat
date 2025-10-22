f2484ba449faff698286ee73beb0890282a73983
mariaaall <vitoriasilvavs4383775@gmail.com>
2025-10-22 17:15:47 -0300

    comit da MV

--- server.py
+++ server.py
@@ -32,7 +32,7 @@ def remove_client(client):
 def main():
   server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-  print("Iniciou o servidor de bate-papo")
+  print("pode conversa")
 
   try:
       server.bind(("localhost", 7777))
